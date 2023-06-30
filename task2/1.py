7f9c900da086cb5e5b66561db115d14e51d4b206
pankaj <pnkjvshsh1@gmail.com>
2023-07-01 02:33:04 +0530

    done

---
+++ task2/1.py
@@ -0,0 +1,8 @@
+
+n = 5
+
+for i in range(1, 6):
+    sum_string = ""
+    for j in range(1, i+1):
+        sum_string += str(j)+" "
+    print(sum_string)
---
+++ task2/ok.js
@@ -0,0 +1,18 @@
+
+
+// let str1 = "PyNaTive";
+// let str2 = "";
+
+// for (let i = 0; i < str1.length; i++) {
+//     if (str1[i].toLowerCase() === str1[i]) {
+//         str2 += str1[i];
+//     }
+// }
+
+// for (let y = 0; y < str1.length; y++) {
+//     if (str1[y].toUpperCase() === str1[y]) {
+//         str2 += str1[y];
+//     }
+// }
+
+// console.log(str2);
